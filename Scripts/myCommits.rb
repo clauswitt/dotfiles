8221553256d38dbd63a34b9a4626acfd4c33b90b
Claus Witt <claus@wittnezz.dk>
2013-01-19 12:38:20 +0100

    allow parameter to set user

--- Scripts/myCommits.rb
+++ Scripts/myCommits.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-myCommits=`git --no-pager log --no-merges --pretty=oneline --author=claus |wc -l`
+myCommits=`git --no-pager log --no-merges --pretty=oneline --author=#{ARGV[0]} |wc -l`
 allCommits=`git --no-pager log --no-merges --pretty=oneline  |wc -l`
 myPercent=myCommits.to_f/allCommits.to_f*100
 
