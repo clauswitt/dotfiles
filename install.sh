bd9da190b7095364cc1e3dd7176960a91c4ad385
Claus Witt <claus@wittnezz.dk>
2013-01-29 09:18:04 +0100

    moved code into functions

--- install.sh
+++ install.sh
@@ -2,6 +2,25 @@
 set -e
 ROOT=${1:-~}
 
+function main {
+  update_git
+  install_packages
+}
+
+function install_packages {
+  install "tmuxinator"
+  install "git/gitconfig" ".gitconfig"
+  install "git/gitignore_global" ".gitignore_global"
+  install "grunt"
+  install "tmux/tmux.conf" ".tmux.conf"
+  install "vim/vimrc" ".vimrc"
+  install "vim/gvimrc" ".gvimrc"
+  install "vim/dotvim" ".vim"
+  install "zsh/oh-my-zsh/plugins/cw" ".oh-my-zsh/plugins/cw"
+  install "zsh/oh-my-zsh/themes/clauswitt.zsh-theme" ".oh-my-zsh/themes/clauswitt.zsh-theme"
+  install "zsh/zshrc" ".zshrc"
+  install "Scripts" "Scripts"
+}
 function install() {
   TARGET=$ROOT/${2:-.$1}
   if [[ -e $TARGET ]]; then
@@ -10,18 +29,8 @@ function install() {
   ln -nfs $PWD/$1 $TARGET
 }
 
-git submodule init
-git submodule update
-
-install "tmuxinator"
-install "git/gitconfig" ".gitconfig"
-install "git/gitignore_global" ".gitignore_global"
-install "grunt"
-install "tmux/tmux.conf" ".tmux.conf"
-install "vim/vimrc" ".vimrc"
-install "vim/gvimrc" ".gvimrc"
-install "vim/dotvim" ".vim"
-install "zsh/oh-my-zsh/plugins/cw" ".oh-my-zsh/plugins/cw"
-install "zsh/oh-my-zsh/themes/clauswitt.zsh-theme" ".oh-my-zsh/themes/clauswitt.zsh-theme"
-install "zsh/zshrc" ".zshrc"
-install "Scripts" "Scripts"
+function update_git {
+  git submodule init
+  git submodule update
+}
+main
