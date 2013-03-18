c6a1d2dce561535e3f8980002bf96b4e68903d9a
Claus Witt <claus@wittnezz.dk>
2013-03-18 10:32:24 +0100

    added handling of email - and minor changes

--- hostfiles/focus.sh
+++ hostfiles/focus.sh
@@ -2,3 +2,4 @@
 rm /etc/hosts
 cp /Users/clauswitt/bin/dotfiles/hostfiles/hosts /etc/
 cat /Users/clauswitt/bin/dotfiles/hostfiles/hosts.focus >> /etc/hosts
+cat /Users/clauswitt/bin/dotfiles/hostfiles/hosts.email >> /etc/hosts
