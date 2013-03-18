26369c1fdb6d18f6c90e9439a43776d73162b859
Claus Witt <claus@wittnezz.dk>
2013-03-18 09:51:45 +0100

    added hostfiles

---
+++ hostfiles/focus.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm /etc/hosts
+cp /Users/clauswitt/bin/dotfiles/hostfiles/hosts /etc/
+cat /Users/clauswitt/bin/dotfiles/hostfiles/hosts.focus >> /etc/hosts
---
+++ hostfiles/procrastinate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm /etc/hosts
+cd /etc
+ln -nfs /Users/clauswitt/bin/dotfiles/hostfiles/hosts
