af806e169eb489700fb5719cf640d5d5fa31dabe
Claus Witt <claus@wittnezz.dk>
2013-07-20 12:31:51 +0200

    removed annoying rake hack

--- zsh/oh-my-zsh/plugins/cw/cw.plugin.zsh
+++ zsh/oh-my-zsh/plugins/cw/cw.plugin.zsh
@@ -331,27 +331,6 @@ CDPATH=.:~/Documents/Projects:~/Documents/Projects/sites:~/Documents/Projects/Sy
 EDITOR=vim
 export EDITOR='mvim -v'
 
-_rake_does_task_list_need_generating () {
-  if [ ! -f .rake_tasks ]; then return 0;
-  else
-    accurate=$(stat -f%m .rake_tasks)
-    changed=$(stat -f%m Rakefile)
-    return $(expr $accurate '>=' $changed)
-  fi
-}
-
-_rake () {
-  if [ -f Rakefile ]; then
-    if _rake_does_task_list_need_generating; then
-      echo "\nGenerating .rake_tasks..." > /dev/stderr
-      rake --silent --tasks | cut -d " " -f 2 > .rake_tasks
-    fi
-    compadd `cat .rake_tasks`
-  fi
-}
-
-compdef _rake rake
-
 sp () {
   PLAYING=`~/Scripts/spotify status|grep playing|wc -l`
   if [[ $PLAYING -eq 1  ]]; then
